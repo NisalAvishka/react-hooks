670c1bf24b3ba0fe9b7c4d13606357f20e7f6c04
Nisal Balasooriya <nisalavishka2001@gmail.com>
2025-11-04 15:18:58 +0530

    usememo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useMemo, useState } from "react";
+import { initialItems } from "./utils/utils";
+
 function App() {
-  return <></>;
+  const[count, setCount] = useState(0);
+  const[items] = useState(initialItems);
+
+  const selectedItem = useMemo(
+    () => items.find((item) => item.isSelected),
+    [items]
+  );
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <h1>Selected Item: {selectedItem?.id}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
 }
 
 export default App;
