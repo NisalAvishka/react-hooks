9eef1a6ae3d721e9a46792e136ffb5cfaf1ac023
Nisal Balasooriya <nisalavishka2001@gmail.com>
2025-11-04 15:09:22 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,5 @@
-import { useState } from "react";
-import { Dashboard } from "./components/Dashboard";
-import { DashboardContext } from "./libs/context";
-
-export interface User{
-  name: string;
-  isSubscribed: boolean;
-}
-
 function App() {
-  const [user] = useState<User>({
-    isSubscribed: true,
-    name: 'You',
-  })
-
-  return (
-    <>
-    <DashboardContext.Provider value={user}>
-      <Dashboard/>
-    </DashboardContext.Provider>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
